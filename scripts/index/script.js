c51fb531a1e3c58f6a8b6211fd19ce84f8073799
Hugo Rios <hugostaq@gmail.com>
2022-02-19 01:35:19 -0300

    Change
    Change

--- scripts/index/script.js
+++ scripts/index/script.js
@@ -7,3 +7,19 @@ document.getElementById("imgSelectBtn").addEventListener("change", e => {
     }
     reader.readAsDataURL(file);
 });
+
+perInfoBtn = document.getElementById("perInfoBtn");
+perInfoBtnFake = document.getElementById("perInfoBtnFake");
+infoInput = document.getElementById("infoInput");
+
+perInfoBtn.addEventListener("click", ()=> {
+    infoInput.style.display = "grid";
+    perInfoBtn.style.display = "none";
+    perInfoBtnFake.style.display = "block"
+});
+
+perInfoBtnFake.addEventListener("click", ()=> {
+    infoInput.style.display = "none";
+    perInfoBtn.style.display = "block";
+    perInfoBtnFake.style.display = "none"
+});
