6b47e28e0b9090e17c09772c29f24c6995275980
Hugo Rios <hugostaq@gmail.com>
2022-02-19 00:26:33 -0300

    Opa
    alou

--- scripts/index/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("imgSelectBtn").addEventListener("change", e => {
-    let file = e.currentTarget.files[0];
-    let reader = new FileReader();
-    reader.onloadend = () => {
-        document.getElementById("charImg").src = reader.result;
-        upload.value = "";
-    }
-    reader.readAsDataURL(file);
-});
