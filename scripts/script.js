46b8d787d6a9a37021ef306c0863f72a88029271
Hugo Rios <hugostaq@gmail.com>
2022-05-14 20:23:32 -0300

    Combat Section Changes
    When One Tab of Combat Section Opens, Any Other Who's Opened, Automatically Closes.

--- scripts/script.js
+++ scripts/script.js
@@ -1041,48 +1041,72 @@ editBtn1.addEventListener('click', ()=> {
     document.getElementById("activeAb").style.display = "block";
     editBtn1.style.display = "none";
     xBtn1.style.display = "block";
-});
 
-xBtn1.addEventListener('click', ()=> {
-    document.getElementById("activeAb").style.display = "none";
-    editBtn1.style.display = "block";
-    xBtn1.style.display = "none";
-});
-
-editBtn2.addEventListener('click', ()=> {
-    document.getElementById("passiveAb").style.display = "block";
-    editBtn2.style.display = "none";
-    xBtn2.style.display = "block";
-});
-
-xBtn2.addEventListener('click', ()=> {
     document.getElementById("passiveAb").style.display = "none";
     editBtn2.style.display = "block";
     xBtn2.style.display = "none";
-});
 
-editBtn3.addEventListener('click', ()=> {
-    document.getElementById("weaponsDiv").style.display = "block";
-    editBtn3.style.display = "none";
-    xBtn3.style.display = "block";
-});
-
-xBtn3.addEventListener('click', ()=> {
     document.getElementById("weaponsDiv").style.display = "none";
     editBtn3.style.display = "block";
     xBtn3.style.display = "none";
-});
 
-editBtn4.addEventListener('click', ()=> {
-    document.getElementById("spellsDiv").style.display = "block";
-    editBtn4.style.display = "none";
-    xBtn4.style.display = "block";
-});
-
-xBtn4.addEventListener('click', ()=> {
     document.getElementById("spellsDiv").style.display = "none";
     editBtn4.style.display = "block";
     xBtn4.style.display = "none";
+});
+
+editBtn2.addEventListener('click', ()=> {
+    document.getElementById("activeAb").style.display = "none";
+    editBtn1.style.display = "block";
+    xBtn1.style.display = "none";
+
+    document.getElementById("passiveAb").style.display = "block";
+    editBtn2.style.display = "none";
+    xBtn2.style.display = "block";
+
+    document.getElementById("weaponsDiv").style.display = "none";
+    editBtn3.style.display = "block";
+    xBtn3.style.display = "none";
+
+    document.getElementById("spellsDiv").style.display = "none";
+    editBtn4.style.display = "block";
+    xBtn4.style.display = "none";
+});
+
+editBtn3.addEventListener('click', ()=> {
+    document.getElementById("activeAb").style.display = "none";
+    editBtn1.style.display = "block";
+    xBtn1.style.display = "none";
+
+    document.getElementById("passiveAb").style.display = "none";
+    editBtn2.style.display = "block";
+    xBtn2.style.display = "none";
+
+    document.getElementById("weaponsDiv").style.display = "block";
+    editBtn3.style.display = "none";
+    xBtn3.style.display = "block";
+
+    document.getElementById("spellsDiv").style.display = "none";
+    editBtn4.style.display = "block";
+    xBtn4.style.display = "none";
+});
+
+editBtn4.addEventListener('click', ()=> {
+    document.getElementById("activeAb").style.display = "none";
+    editBtn1.style.display = "block";
+    xBtn1.style.display = "none";
+
+    document.getElementById("passiveAb").style.display = "none";
+    editBtn2.style.display = "block";
+    xBtn2.style.display = "none";
+
+    document.getElementById("weaponsDiv").style.display = "none";
+    editBtn3.style.display = "block";
+    xBtn3.style.display = "none";
+
+    document.getElementById("spellsDiv").style.display = "block";
+    editBtn4.style.display = "none";
+    xBtn4.style.display = "block";
 });
 
 function changeSize1() {
